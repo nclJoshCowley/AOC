dd6a9fe7f56add44f44d1a1b6f4d408ce1c7f4ef
Josh Cowley <j.cowley1@ncl.ac.uk>
2021-12-07 14:26:07 +0000

    Added solution - 07a

---
+++ Q07/Q07a_solution.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author:   Josh Cowley
+@email:    j.cowley1@ncl.ac.uk
+"""
+
+# %% Dependencies
+import numpy as np
+# import functools
+    
+# %% Read from file
+positions_eg = [16,1,2,0,4,2,7,1,2,14]
+    
+with open('Q07_data.txt', 'r') as file:
+    positions = [int(i) for i in file.read().split(',')]
+
+# %%
+def dist(x, n):
+    return sum([abs(xi - n) for xi in x])
+
+# %%
+def brute_force(x):
+    out = [dist(x, ni) for ni in range(max(x))]
+    return np.argmin(out), out
+        
+
+# %%
+if __name__ == '__main__':
+    arg, lst = brute_force(positions_eg)
+    print(f"Position = {arg:4.0f}, Fuel = {lst[arg]:4.0f}")
+
+    arg, lst = brute_force(positions)
+    print(f"Position = {arg:4.0f}, Fuel = {lst[arg]:4.0f}")
+    
