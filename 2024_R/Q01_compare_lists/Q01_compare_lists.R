261802835923e695b19d7b06f9bbbf94769a4e90
Josh Cowley <j.cowley1@ncl.ac.uk>
2024-12-02 19:50:13 +0000

    Omitted puzzle inputs from git

--- 2024_R/Q01_compare_lists/Q01_compare_lists.R
+++ 2024_R/Q01_compare_lists/Q01_compare_lists.R
@@ -5,11 +5,11 @@ input <- utils::read.table("Q01_input.txt")
 input$V1 <- sort(input$V1)
 input$V2 <- sort(input$V2)
 
-# Q1.a) Total distance between sorted lists
+# Q01.a) Total distance between sorted lists
 all_distances <- abs(input$V1 - input$V2)
 print(sum(all_distances))
 
-# Q1.a) Similarity scores
+# Q01.a) Similarity scores
 n_matches <-
   vapply(input$V1, function(.x) sum(.x == input$V2), numeric(1))
 
