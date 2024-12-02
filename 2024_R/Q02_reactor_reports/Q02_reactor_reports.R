d6c7c8e35350575c8afa95c312da3e3e4f83fc24
Josh Cowley <j.cowley1@ncl.ac.uk>
2024-12-02 20:43:48 +0000

    Added Q2 - Part 2 solution

--- 2024_R/Q02_reactor_reports/Q02_reactor_reports.R
+++ 2024_R/Q02_reactor_reports/Q02_reactor_reports.R
@@ -20,3 +20,35 @@ is_safe <-
   vapply(function(.x) all(dplyr::between(abs(.x), 1, 3)), logical(1))
 
 print(sum(is_monotonic & is_safe))
+
+# Q02.b)
+# Introduction of problem dampener
+# (If removing 1 element makes sequence safe, it's safe!)
+
+# Brute force approach. Iterative looping unlikely to be quick
+
+# Function to implement logic from part a)
+is_report_safe <- function(x) {
+  x_diff <- diff(x)
+
+  is_monotonic <- length(unique(sign(x_diff))) == 1
+  is_safe_jump <- all(dplyr::between(abs(x_diff), 1, 3))
+
+  return(is_monotonic & is_safe_jump)
+}
+
+# Function to implement new logic from part b)
+is_report_dampened_safe <- function(x) {
+  if (is_report_safe(x)) return(TRUE)
+
+  for (i in seq_along(x)) {
+    if (is_report_safe(x[-i])) return(TRUE)
+  }
+
+  return(FALSE)
+}
+
+# Apply to all inputs
+output <- vapply(input_seq, is_report_dampened_safe, logical(1))
+
+print(sum(output))
